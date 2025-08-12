40074ee832e8119adff55be2fd1fd68ba54117a3
AL-Dos <barbarianclaw1@gmail.com>
2025-08-12 18:39:11 +0800

    feat: added buttons for clear and delete

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,10 +4,10 @@ import javax.swing.*;
 
 public class Calculator implements ActionListener{
     JFrame frame;
-    JTextField textfield;
+    JTextField textField;
     JButton[] numberKeypad = new JButton[10];
     JButton[] logicKeypad = new JButton[8];
-    JButton[] addB, subB, mulB, divB, decB, eqB, delB, clrB;
+    JButton addB, subB, mulB, divB, decB, eqlB, delB, clrB;
     JPanel panel;
 
     Font currentFont = new Font("Monospaced", Font.BOLD, 50);
@@ -19,6 +19,48 @@ public class Calculator implements ActionListener{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(450,600);
         frame.setLayout(null);
+
+        textField = new JTextField();
+        textField.setBounds(43,50,350,70);
+        textField.setFont(currentFont);
+        textField.setEditable(false);
+
+        addB = new JButton("+");
+        subB = new JButton("-");
+        mulB = new JButton("*");
+        divB = new JButton("/");
+        decB = new JButton(".");
+        eqlB = new JButton("=");
+        delB = new JButton("del");
+        clrB = new JButton("clr");
+        logicKeypad[0] = addB;
+        logicKeypad[1] = subB;
+        logicKeypad[2] = mulB;
+        logicKeypad[3] = divB;
+        logicKeypad[4] = decB;
+        logicKeypad[5] = eqlB;
+        logicKeypad[6] = delB;
+        logicKeypad[7] = clrB;
+
+        for (int i = 0; i < 8; i++) {
+            logicKeypad[i].addActionListener(this);
+            logicKeypad[i].setFont(currentFont);
+            logicKeypad[i].setFocusable(false);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            numberKeypad[i] = new JButton(String.valueOf(i));
+            numberKeypad[i].addActionListener(this);
+            numberKeypad[i].setFont(currentFont);
+            numberKeypad[i].setFocusable(false);            
+        }
+
+        delB.setBounds(50,430,145,50);
+        clrB.setBounds(250,430,145,50);
+
+        frame.add(delB);
+        frame.add(clrB);
+        frame.add(textField);
         frame.setVisible(true);
     }
     public static void main(String[] args) throws Exception {
